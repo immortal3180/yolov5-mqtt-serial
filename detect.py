154f17768786c980b85aa267e3b561164d9a0a2c
immortal3180 <immortal3180.github.io>
2026-01-05 01:37:49 +0800

    修改：提高默认置信度阈值减少候选框 修改：默认启用 --view-img

--- detect.py
+++ detect.py
@@ -484,11 +484,11 @@ def parse_opt():
     parser.add_argument("--source", type=str, default=ROOT / "data/images", help="file/dir/URL/glob/screen/0(webcam)")
     parser.add_argument("--data", type=str, default=ROOT / "data/coco128.yaml", help="(optional) dataset.yaml path")
     parser.add_argument("--imgsz", "--img", "--img-size", nargs="+", type=int, default=[320], help="inference size h,w")
-    parser.add_argument("--conf-thres", type=float, default=0.25, help="confidence threshold")
+    parser.add_argument("--conf-thres", type=float, default=0.5, help="confidence threshold")
     parser.add_argument("--iou-thres", type=float, default=0.45, help="NMS IoU threshold")
     parser.add_argument("--max-det", type=int, default=100, help="maximum detections per image")
     parser.add_argument("--device", default="", help="cuda device, i.e. 0 or 0,1,2,3 or cpu")
-    parser.add_argument("--view-img", action="store_true", help="show results")
+    parser.add_argument("--view-img", action="store_true", default=True, help="show results (default: enabled)")
     parser.add_argument("--save-txt", action="store_true", help="save results to *.txt")
     parser.add_argument(
         "--save-format",
